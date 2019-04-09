39437175989472af951cf3022e5c8094c54cad75
chenpeng <764762964@qq.com>
2019-04-09 21:50:56 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         singletonPattern instance = singletonPattern.INSTANCE;
         instance.say();
+//chenpeng
         System.out.println("Hello World!");
     }
 }
